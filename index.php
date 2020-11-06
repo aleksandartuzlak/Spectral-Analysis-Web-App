dac6b7289a8711b78c856e8315fed1023f4950e7
aleksandartuzlak <63924429+aleksandartuzlak@users.noreply.github.com>
2020-11-06 17:28:43 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aleksandar Tuzlak</title>
+    <link rel="shortcut icon" href="#">
+
+    
+    <!-- FAVICON -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
+    <meta name="apple-mobile-web-app-title" content="AT">
+    <meta name="application-name" content="AT">
+    <meta name="msapplication-TileColor" content="#9f00a7">
+    <meta name="theme-color" content="#000000">
+
+    <link rel="stylesheet" href="myWebsite.css">
+
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,100,400" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <main>
+        <!-- NAVBAR -->
+        <header>
+            <nav class="nav-wrapper transparent z-depth-0">
+                <div class="container">
+                    <a href="" class="sidenav-trigger" id="L" data-target='mobile-menu'>
+                        <i class="material-icons">menu</i>
+                    </a>
+                    <ul class="right hide-on-small-and-down white-text">
+                        <li><a href="#projectbox" id="L">Projects</a></li>
+                        <li><a href="#contact" id="L">Contact</a></li>
+                        <li><a href="https://www.instagram.com/aleksandar.tuzlak/?hl=de" class="tooltipped btn-floating btn-small transparent z-depth-0" data-tooltip="Instagram" target="_blank">
+                            <i class="fab fa-instagram" id="L"></i>
+                        </a></li>
+                        <li><a href="https://www.linkedin.com/in/aleksandar-tuzlak-381a941b4/" class="tooltipped btn-floating btn-small transparent z-depth-0 " data-tooltip="LinkedIn" target="_blank">
+                            <i class="fab fa-linkedin" id="L"></i>
+                        </a></li>
+                        <li><a href="https://github.com/aleksandartuzlak" class="tooltipped btn-floating btn-small transparent z-depth-0" data-tooltip="Github" target="_blank">
+                            <i class="fab fa-github" id="L"></i>
+                        </a></li>
+                    </ul>
+                    <ul class="sidenav" id="mobile-menu">
+                        <li><a href="#projectbox" class="white-text">Projects</a></li>
+                        <li><a href="#contact" class="white-text">Contact</a></li>
+                        <li><a href="https://www.instagram.com/aleksandar.tuzlak/?hl=de" target="_blank" class="white-text">Instagram</a></li>
+                        <li><a href="https://www.linkedin.com/in/aleksandar-tuzlak-381a941b4/" target="_blank" class="white-text">LinkedIn</a></li>
+                        <li><a href="https://github.com/aleksandartuzlak/Lambda-Spectral-Analysis" target="_blank" class="white-text">Github</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
+        <div class="container section">
+            <div class="row">
+                <div class="col s12 l12" id="block">
+                </div>
+            </div>
+        </div>
+
+        <div class="container section">
+            <div class="row">
+                <div class="col s12 l12" id="nameAn">
+                    <div class="data">Aleksandar Tuzlak</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container section">
+            <div class="row">
+                <div class="col s12 l12" id="block2">
+                </div>
+            </div>
+        </div>
+
+        <div class="container section">
+            <div class="row">
+                <div class="col s12 l12">
+                    <h6 class="center white-text">Hi, I'm a 19 year old student from Switzerland.</h6>
+                </div>
+            </div>
+        </div>
+        
+          <!-- projects -->
+          
+          <div class="container section">
+              <div class="row">
+                  <div class="col s12 l12">
+                      <h5 class="projects" id="h5">Latest Projects</h5>
+                    </div>
+                </div>
+            </div>        
+            
+            <div class="container section" id="projectbox">
+                <div class="row">
+                    <div class="col s12 l12">
+                        <div class="card z-depth-3" id="bc">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" src="staticAnalysis.png">
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title" id="M">Spectral Analysis Webtool<i class="material-icons right">more_vert</i></span>
+                                <a class="waves-effect waves-light btn modal-trigger" href="select.html" id="M">Check it out!</a>
+                            </div>
+                            <div class="card-reveal" id="bc">
+                                <span class="card-title" id="bc">Spectral Analysis Webtool<i class="material-icons right">close</i></span>
+                                <p>This Webtool was mainly created to make it faster to create spectral tables aswell as to display the intensities of spectral lines for further scientific purposes or just to have fun...</p>
+                            </div>
+                        </div>       
+                    </div>
+                </div>
+            </div>
+        
+
+
+            <div class="container section">
+                <div class="row">
+                <div class="col s12 l12">
+                    <h5 class="projects" id="L">My Skills</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="container" id='skills'>
+            <div class="row">
+                <div class="col s12 l12">
+                    <div class="col s3 l3">
+                        <div id="iconbox">
+                        <i class="fab fa-html5 fa-6x" style="color: #F16529;, margin-right: 50%;"></i>
+                        </div>
+                    </div>
+                    <div class="col s3 l3">
+                        <div id="iconbox">
+                            <i class="fab fa-css3-alt fa-6x" style="color: #264de4;"></i>
+
+                        </div>
+                    </div>
+                    <div class="col s3 l3">
+                        <div id="iconbox">
+                            <i class="fab fa-js fa-6x" style="color: #f0db4f;"></i>
+
+                        </div>
+                    </div>
+                    <div class="col s3 l3">
+                        <div id="iconbox">
+                            <i class="fab fa-python fa-6x" style="color: white;"></i>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- skills -->
+
+
+        <!-- contact -->
+        
+<section class="section container scrollspy" id="contact">
+    <div class="row">
+        <div class="col s12 l5">
+            <h4 id="h5">
+                Contact me!
+            </h4>
+            <p id="L">Feel free to contact me at any time. I'll try to respond as fast as possible. You may also contact me over my social media accounts.</p>
+        </div>
+        <div class="col s12 l5 offset-l2">
+            <br><br>
+            <form action="contactform.php" method="post">
+                <div class="input-field white-text">
+                    <i class="material-icons prefix white-text">email</i>
+                    <input type="email" name="mail" id="email" class="white-text">
+                    <label for="email" class="white-text">Your Email</label>
+                </div>
+                <div class="input-field white-text">
+                    <i class="material-icons prefix white-text">message</i>
+                    <textarea id="message" name="message" class="materialize-textarea white-text" cols="30" rows="10"></textarea>
+                    <label for="message" class="white-text">Your Message</label>
+                </div>
+                <div class="input-field center">
+                    <button class="btn z-depth-0" type="submit" name="submit" id="K">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
+
+
+    </main>
+
+    <!-- footer -->
+    <footer class="page-footer grey darken-3">
+        <div class="container">
+            <div class="row">
+                <div class="col s12 l4">
+                    <h5>
+                        Navigation
+                    </h5>
+                    <ul>
+                        <li><a href="#projectbox" id="L">Projects</a></li>
+                        <li><a href="#skills" id="L">Skills</a></li>
+                        <li><a href="#contact" id="L">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div class="col s12 l4">
+                    <h5>E-Mail</h5>
+                    <ul>
+                        <li><a href="mailto: aleksandar_tuzlak@hotmail.com">aleksandar_tuzlak@hotmail.com</a></li>
+                    </ul>
+                </div>
+
+                <div class="col s12 l4">
+                    <h5>Get Connected</h5>
+                    <ul>
+    
+                        <li><a href="https://www.instagram.com/aleksandar.tuzlak/?hl=de" class="tooltipped btn-floating btn-small transparent z-depth-0" data-tooltip="Instagram" data-position="right" target="_blank">
+                            <i class="fab fa-instagram" id="L"></i>
+                        </a></li>
+                        <li><a href="https://www.linkedin.com/in/aleksandar-tuzlak-381a941b4/" class="tooltipped btn-floating btn-small transparent z-depth-0 " data-tooltip="LinkedIn" data-position="right" target="_blank">
+                            <i class="fab fa-linkedin" id="L"></i>
+                        </a></li>
+                        <li><a href="https://github.com/aleksandartuzlak" class="tooltipped btn-floating btn-small transparent z-depth-0" data-tooltip="Github" data-position="right" target="_blank">
+                            <i class="fab fa-github" id="L"></i>
+                        </a></li>
+                    </ul>
+                </div>
+            </div>
+            
+            </div>
+        </div>
+        <div class="footer-copyright grey darken-4">
+            <div class="container center-align">
+                <ul>
+                    <li>
+                        <a class="hosttech" href="http://www.hosttech.ch/webhosting/special?promocode=30266452" target="_blank" title="Webhosting" ><img src="logo_100x40-oekopower.gif" alt="Webhosting"/></a> 
+                    </li>
+
+                </ul>
+                &copy;2020 Aleksandar Tuzlak
+            </div>
+        </div>
+    </footer>
+
+    <!-- SCRIPTS -->
+
+     <!-- jQuery -->
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+     
+     <!-- Compiled and minified JavaScript -->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+     <!-- baffle.js -->
+    <script src="https://cdn.jsdelivr.net/npm/baffle@0.3.6/dist/baffle.min.js"></script>
+
+     <script src="myWebsite.js"></script>
+
+
+</body>
+</html>
+
